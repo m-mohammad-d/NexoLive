8ccad118caf3d3b698b5a3f08be17273dfe4a9bd
silvershade1000@gmail.com <silvershade1000@gmail.com>
2025-10-10 20:01:37 +0330

    feat: implement Navbar with Logo, Actions, and Search components; update layout structure

---
+++ src/app/(browse)/_components/navbar/index.tsx
@@ -0,0 +1,12 @@
+import { Actions } from "./actions";
+import { Logo } from "./Logo";
+import { Search } from "./search";
+export const Navbar = () => {
+  return (
+    <nav className="fixed top-0 left-0 w-full h-20 bg-[#252731] z-49 px-2 lg:px-4 flex items-center justify-between shadow-md">
+      <Logo />
+      <Search />
+      <Actions />
+    </nav>
+  );
+};
