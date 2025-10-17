0cbfdc6b7f9f0a8c232778dcb8af32a19ba29a13
silvershade1000@gmail.com <silvershade1000@gmail.com>
2025-10-17 12:04:49 +0330

    feat: enhance sidebar with following and recommended users; add skeleton loading states

--- src/app/(browse)/_components/sidebar/index.tsx
+++ src/app/(browse)/_components/sidebar/index.tsx
@@ -1,16 +1,21 @@
+import { getFollwedUser } from "@/service/follow-service";
 import { getRecommended } from "@/service/recommended-service";
+import React from "react";
+import { Following, FollowingSkeleton } from "./following";
 import { Recommended, RecommendedSkeleton } from "./recommended";
 import { Toggle, ToggleSkeleton } from "./Toggle";
 import { Wrapper } from "./Wrapper";
 
+
 export async function Sidebar() {
   const recommended = await getRecommended();
+  const following = await getFollwedUser();
 
   return (
     <Wrapper>
       <Toggle />
-
       <div className="space-y-4 pt-4 lg:pt-0">
+        <Following data={following} />
         <Recommended data={recommended} />
       </div>
     </Wrapper>
@@ -21,6 +26,7 @@ export function SidebarSkeleton() {
   return (
     <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35 z-50]">
       <ToggleSkeleton />
+      <FollowingSkeleton />
       <RecommendedSkeleton />
     </aside>
   );
