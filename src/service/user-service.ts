08d95ada2dbe04118fd917c4106f304a41fbf4b4
silvershade1000@gmail.com <silvershade1000@gmail.com>
2025-11-01 19:57:09 +0330

    feat: add viewer token creation and management with JWT decoding for stream access

--- src/service/user-service.ts
+++ src/service/user-service.ts
@@ -11,12 +11,34 @@ export const getUserByUsername = async (username: string) => {
       username: true,
       bio: true,
       imageUrl: true,
+      stream: {
+        select: {
+          id: true,
+          isLive: true,
+          isChatDelayed: true,
+          isChatEnabled: true,
+          isChatFollowersOnly: true,
+          thumbnailUrl: true,
+          name: true,
+        },
+      },
       _count: {
         select: {
           followedBy: true,
         },
       },
     },
+  });
+
+  return user;
+};
+
+export const getUserById = async (id: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      id,
+    },
+    include: { stream: true },
   });
 
   return user;
