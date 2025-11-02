7d3d3b5c6881d422538785c185baf402ab9b753b
silvershade1000@gmail.com <silvershade1000@gmail.com>
2025-11-02 18:19:12 +0330

    feat: implement chat functionality with chat toggle, chat form, and chat messages display

--- src/components/stream-player/index.tsx
+++ src/components/stream-player/index.tsx
@@ -4,7 +4,11 @@ import { useViewerToken } from "@/hooks/useVieverToken";
 
 import { LiveKitRoom } from "@livekit/components-react";
 import { cn } from "@/lib/utils";
+import { useChatSidebar } from "@/store/use-chat-sidebar";
+
 import { Video } from "./Video";
+import { Chat } from "./Chat";
+import { ChatToggle } from "./ChatToggle";
 type CustomStream = {
   id: string;
   isChatEnabled: boolean;
@@ -36,17 +40,36 @@ export function StreamPlayer({
   isFollowing: boolean;
 }) {
   const { identity, name, token } = useViewerToken(user.id);
+  const { collapsed } = useChatSidebar((state) => state);
+
   return (
     <>
+      {collapsed && (
+        <div className="hidden lg:block fixed top-[100px] right-2 z-50">
+          <ChatToggle />
+        </div>
+      )}
       <LiveKitRoom
         token={token}
         serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
         className={cn(
-          "grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+          "grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full",
+          collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2"
         )}
       >
         <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
           <Video hostName={user.username} hostIdentity={user.id} />
+        </div>
+        <div className={cn("col-span-1", collapsed && "hidden")}>
+          <Chat
+            viewerName={name}
+            hostName={user.username}
+            hostIdentity={user.id}
+            isFollowing={isFollowing}
+            isChatEnabled={stream.isChatEnabled}
+            isChatDelayed={stream.isChatDelayed}
+            isChatFollowersOnly={stream.isChatFollowersOnly}
+          />
         </div>
       </LiveKitRoom>
     </>
