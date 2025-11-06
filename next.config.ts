1609e30a6a60a3c826018125b9f62738d462630a
silvershade1000@gmail.com <silvershade1000@gmail.com>
2025-11-06 10:39:41 +0330

    update next js config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,33 @@
 import withSerwistInit from "@serwist/next";
 
+const revision = crypto.randomUUID();
+
 const withSerwist = withSerwistInit({
   swSrc: "src/app/sw.ts",
   swDest: "public/sw.js",
   cacheOnNavigation: true,
   register: true,
   reloadOnOnline: true,
+  dontCacheBustURLsMatching:
+    /^\/_next\/static\/(?:chunks|css)\/.*\.(?:js|css)$/,
   maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
   disable: process.env.NODE_ENV === "development",
+  exclude: [
+    /\.map$/,
+    /^manifest$/,
+    /\.txt$/,
+    /\.xml$/,
+    /\.json$/,
+    /^_next\/static\/chunks\/pages\/_error/,
+  ],
+  additionalPrecacheEntries: [
+    { url: "/", revision },
+    { url: "/search", revision },
+    { url: "/offline", revision },
+    { url: "/manifest.webmanifest", revision },
+    { url: "/favicon.ico", revision },
+    { url: "/manifest.json", revision },
+  ],
 });
 
 export default withSerwist({
