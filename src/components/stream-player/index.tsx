b6a26e0e484e61191893dfd72b5f2c69731b5c08
silvershade1000@gmail.com <silvershade1000@gmail.com>
2025-11-06 13:40:57 +0330

    feat :add loading for user

--- src/components/stream-player/index.tsx
+++ src/components/stream-player/index.tsx
@@ -6,10 +6,10 @@ import { LiveKitRoom } from "@livekit/components-react";
 import { cn } from "@/lib/utils";
 import { useChatSidebar } from "@/store/use-chat-sidebar";
 
-import { Video } from "./Video";
-import { Chat } from "./Chat";
+import { Video, VideoSkeleton } from "./Video";
+import { Chat, ChatSkeleton } from "./Chat";
 import { ChatToggle } from "./ChatToggle";
-import { Header } from "./Header";
+import { Header, HeaderSkeleton } from "./Header";
 import { InfoCard } from "./InfoCard";
 import { AboutCard } from "./AboutCard";
 type CustomStream = {
@@ -97,5 +97,19 @@ export function StreamPlayer({
         </div>
       </LiveKitRoom>
     </>
+  );
+}
+
+export function StreamPlayerSkeleton() {
+  return (
+    <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
+      <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+        <VideoSkeleton />
+        <HeaderSkeleton />
+      </div>
+      <div className="col-span-1 bg-background">
+        <ChatSkeleton />
+      </div>
+    </div>
   );
 }
